e279b2f9c9f5b3a0f6fe7852e319d28b36625245
Kevin Stine <ikaikastine@gmail.com>
2016-04-04 09:35:41 -0700

    added patterm matching function

--- tutorial/functions.hs
+++ tutorial/functions.hs
@@ -5,3 +5,10 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
   then x
   else x*2
+
+-- Pattern Matching Functions
+
+mySum :: [Int] -> Int
+mySum [] = 0
+mySum (x:xs) = x + mySum xs
+
